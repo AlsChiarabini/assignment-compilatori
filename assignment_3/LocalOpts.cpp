f0bec96a775b808cf09f6e07aeea5cb267557c42
pietro <pietrofoggia03@gmail.com>
2025-04-29 04:43:03 -0500

    add temporary third ass

---
+++ assignment_3/LocalOpts.cpp
@@ -0,0 +1,57 @@
+#include "llvm/IR/Function.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/PassPlugin.h"
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/Analysis/LoopInfo.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/Dominators.h"
+
+using namespace llvm;
+
+namespace{
+
+struct MyFunctionPass : PassInfoMixin<MyFunctionPass> {
+  PreservedAnalyses run(Function &F, FunctionAnalysisManager &AM) {
+    if (F.isDeclaration())
+      return PreservedAnalyses::all();
+
+    errs() << "Running FunctionPass on: " << F.getName() << "\n";
+
+    // Ottieni l'analisi LoopInfo
+    LoopInfo &LI = AM.getResult<LoopAnalysis>(F);
+
+    //Per sapere se la funzione/CFG abbia almeno un loop: 
+    if (!LI.empty()) {
+  	errs() << "Function " << F.getName() << " contains at least one loop.\n";
+    } else {
+  	errs() << "Function " << F.getName() << " does NOT contain any loops.\n";
+    }
+
+    
+
+    return PreservedAnalyses::all();
+  }
+
+  static bool isRequired() { return true; }
+};
+
+} // namespace
+
+// Plugin registration
+extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo llvmGetPassPluginInfo() {
+  return {
+    LLVM_PLUGIN_API_VERSION, "MyFunctionPass", LLVM_VERSION_STRING,
+    [](PassBuilder &PB) {
+      PB.registerPipelineParsingCallback(
+        [](StringRef Name, FunctionPassManager &FPM,
+           ArrayRef<PassBuilder::PipelineElement>) {
+          if (Name == "myLICM") {
+            FPM.addPass(MyFunctionPass());
+            return true;
+          }
+          return false;
+        });
+    }
+  };
+}
+
