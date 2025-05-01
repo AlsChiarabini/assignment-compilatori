baac8994812edf1dae0bc2adedbfcda3a7479308
pietro <pietrofoggia03@gmail.com>
2025-05-01 07:29:44 -0500

    added nested loop tests

---
+++ assignment_3/test6.cpp
@@ -0,0 +1,15 @@
+int main () {
+   int a = 1;
+   int b = 2;
+    
+	
+   for (int i = 0; i < 10; i++) {
+    	for (int j = 0; j < 10 ; j++){
+    		int var = a * i;
+    	}
+       
+    }
+  //qui ci basta che l'istruzioni val al pre-header del primo ciclo
+
+    return 0;
+}
---
+++ assignment_3/test7.cpp
@@ -0,0 +1,15 @@
+int main () {
+   int a = 1;
+   int b = 2;
+    
+	
+   for (int i = 0; i < 10; i++) {
+    	for (int j = 0; j < 10 ; j++){
+    		int var = a * b;
+    	}
+       
+    }
+  // qui invece deve andare al preader del ciclo esterno!
+
+    return 0;
+}
